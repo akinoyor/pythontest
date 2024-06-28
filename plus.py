7b3a21b93fb450ed89dbfbe79cd93833199b2e0f
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-06-28 21:37:34 +0900

    add Four arithmetic operations

---
+++ plus.py
@@ -0,0 +1,7 @@
+num1 = float(input("1つ目の数字を入力してください: "))
+num2 = float(input("2つ目の数字を入力してください: "))
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
