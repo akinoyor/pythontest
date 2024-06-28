d34dc1b2a12b8128680ad397dae3c3c4d87c75c9
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-06-28 22:24:57 +0900

    memo

--- plus.py
+++ plus.py
@@ -5,3 +5,24 @@ print(f"{num1} + {num2} = {num1 + num2}")
 print(f"{num1} - {num2} = {num1 - num2}")
 print(f"{num1} * {num2} = {num1 * num2}")
 print(f"{num1} / {num2} = {num1 / num2}")
+
+s = 'abc'
+#インデント？
+print(f'right : {s:*>8}')
+print(f'center: {s:*^8}')
+print(f'left  : {s:*<8}')
+
+i = 255
+# ゼロ埋め
+print(f'zero padding: {i:08}')
+# zero padding: 00001234
+
+# 2 8 16進数
+print(f'bin       : {i:08b}')
+print(f'oct       : {i:08o}')
+print(f'hex(lower): {i:08x}')
+print(f'hex(upper): {i:08X}')
+# bin       : 11111111
+# oct       : 00000377
+# hex(lower): 000000ff
+# hex(upper): 000000FF
